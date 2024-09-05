ec4555869ab75be1049fc5ba7f29fdab40d1f551
Omkar Potphode <omkar03potphode@gmail.com>
2024-09-05 18:26:13 +0530

    ✨ Added the main title with gradient colors

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("Hello world!!!!")
+#!/usr/bin/env node 
+
+import figlet from "figlet";
+import gradient from "gradient-string";
+
+console.clear();
+
+//Create a colorful gradient for main title
+const titleGradient = gradient("cyan", "magenta", "yellow");
+
+//Generate the main title using figlet
+figlet(
+    "Omkar Potphode",
+    {
+        font: "Slant",
+        horizontalLayout: "default",
+        verticalLayout: "default",
+    },
+    function (err, data){
+        if(err){
+            console.log("Something went wrong!!!");
+            console.dir(err);
+            return;
+        } else {
+            console.log(titleGradient(data));
+        }
+    }
+)
